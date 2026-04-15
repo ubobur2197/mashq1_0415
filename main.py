e44e35d1c6a6eae57c8dbd3bb4d00634eb3b166f
Bobur <ubobur2197@gmail.com>
2026-04-15 11:19:18 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# 1
+roy = [0, 1, 2, 3]
+natija = [x + 10 for x in roy]
+print(natija)
+
+# 2
+roy = ["x", "y", "z"]
+natija = ["harf: " + x for x in roy]
+print(natija)
+
+# 3
+roy = [50, 100, 150]
+natija = [x / 2 for x in roy]
+print(natija)
+
+# 4
+roy = ["5", "6", "7"]
+natija = [int(x) ** 2 for x in roy]
+print(natija)
+
+# 5
+roy = [8, 9, 10]
+natija = [x ** 2 for x in roy]
+print(natija)
